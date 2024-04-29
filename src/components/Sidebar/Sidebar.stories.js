3f96ebb7604ee6d4edebde5258e0b18112bc95cd
clamba15 <cl@customcraftsoftware.com>
2024-04-29 11:18:12 -0700

    testing 0.0.2

--- src/components/Sidebar/Sidebar.stories.js
+++ src/components/Sidebar/Sidebar.stories.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Sidebar from './Sidebar'; 
 import SidebarItem from '../SidebarItem/SidebarItem';
+import Row from '../Row/Row';
+import Col from '../Col/Col';
+import Container from '../Container/Container';
+import Box from '../Box/Box';
 
 export default {
   title: 'Components/Sidebar', 
@@ -13,11 +17,20 @@ export default {
 
 export const Default = () => {
   return (   
-    <Sidebar>
-      <SidebarItem>1</SidebarItem>
-      <SidebarItem>2</SidebarItem>
-      <SidebarItem>3</SidebarItem>
-      <SidebarItem>4</SidebarItem>
-    </Sidebar>
+    <Container>
+      <Row>
+        <Col>
+          <Sidebar>
+            <SidebarItem>1</SidebarItem>
+            <SidebarItem>2</SidebarItem>
+            <SidebarItem>3</SidebarItem>
+            <SidebarItem>4</SidebarItem>
+          </Sidebar>
+        </Col>
+        <Col>
+          <Box bgColor="#004400">hello</Box>
+        </Col>
+      </Row>
+    </Container>
   );
 }
