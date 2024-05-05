a616e057d17c8ad015fce2912a3c987cc56d71da
clamba15 <cl@customcraftsoftware.com>
2024-05-04 19:12:45 -0700

    sidebar toogle

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
 
 interface SidebarProps { 
@@ -8,16 +8,34 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ shadowColor = 'rgba(0,0,0,0.1)', bgColor = 'rgba(255,255,255,1.0)', children }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   const style: React.CSSProperties = {
     backgroundColor: bgColor,
-    boxShadow: `0 4px 8px ${shadowColor}`
+    boxShadow: `0 4px 8px ${shadowColor}`,
+    display: isOpen ? 'none' : 'flex',
   }
 
+  const toggleButtonStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: '10px',
+    left: '10px',
+  };
+
   return (
-    <div className="sidebar" style={style}>
-        <ul>
-            {children}
-        </ul>
+    <div>
+      <button onClick={toggleSidebar} style={toggleButtonStyle}>
+        {isOpen ? 'Close Sidebar' : 'Open Sidebar'}
+      </button>
+      <div className="sidebar" style={style}>
+          <ul>
+              {children}
+          </ul>
+      </div>
     </div>
   );
 };
