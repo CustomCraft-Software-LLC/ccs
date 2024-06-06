94d62e9d5e74a2b852f88287964d5b1c8dd1f0b8
clamba15 <cl@customcraftsoftware.com>
2024-06-05 19:51:46 -0700

    api

--- packages/utils/src/utils/api.ts
+++ packages/utils/src/utils/api.ts
@@ -1,18 +1,39 @@
-export const getRequest = async (api_url : string, endpoint : string, data : JSON, headers : JSON, token : number) => { 
+type header = Record<string, any>;
+
+const isValidURL = (url: string) : boolean => { 
+    try { 
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+const isValidEndpoint = (endpoint: any) : boolean => {
+    return typeof endpoint === 'string' && endpoint.trim().length > 0;
+}
+
+export const getRequest = async (api_url : string, endpoint : string, headers : header, token : number) => { 
+    if(!isValidURL(api_url)) throw new Error('Invalid API URL');
+    if(!isValidEndpoint(endpoint)) throw new Error('Invalid endpoint');
+
     const request = await fetch(`${api_url}/${endpoint}`, { 
         method: 'get',
         headers: headers,
-        body: JSON.stringify(data),
     });
 
     if (!request.ok) {
-        throw new Error(await request.json().message || 'Fetch failed');
+        const error = await request.json();
+        throw new Error(error || 'Fetch failed');
     }
 
     return request.json();
 } 
 
-export const postRequest = async (api_url : string, endpoint : string, data : JSON, headers : JSON, token : number) => { 
+export const postRequest = async (api_url : string, endpoint : string, data : JSON, headers : header, token : number) => { 
+    if(!isValidURL(api_url)) throw new Error('Invalid API URL');
+    if(!isValidEndpoint(endpoint)) throw new Error('Invalid endpoint');
+
     const request = await fetch(`${api_url}/${endpoint}`, { 
         method: 'POST',
         headers: headers,
@@ -20,7 +41,43 @@ export const postRequest = async (api_url : string, endpoint : string, data : JS
     });
 
     if (!request.ok) {
-        throw new Error(await request.json().message || 'Fetch failed');
+        const error = await request.json();
+        throw new Error(error || 'Fetch failed');
+    }
+
+    return request.json();
+} 
+
+export const putRequest = async (api_url : string, endpoint : string, data : JSON, headers : header, token : number) => { 
+    if(!isValidURL(api_url)) throw new Error('Invalid API URL');
+    if(!isValidEndpoint(endpoint)) throw new Error('Invalid endpoint');
+
+    const request = await fetch(`${api_url}/${endpoint}`, { 
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+    });
+
+    if (!request.ok) {
+        const error = await request.json();
+        throw new Error(error || 'Fetch failed');
+    }
+
+    return request.json();
+} 
+
+export const deleteRequest = async (api_url : string, endpoint : string, headers : header, token : number) => { 
+    if(!isValidURL(api_url)) throw new Error('Invalid API URL');
+    if(!isValidEndpoint(endpoint)) throw new Error('Invalid endpoint');
+
+    const request = await fetch(`${api_url}/${endpoint}`, { 
+        method: 'POST',
+        headers: headers,
+    });
+
+    if (!request.ok) {
+        const error = await request.json();
+        throw new Error(error || 'Fetch failed');
     }
 
     return request.json();
