d3d4512e996dd150f898df1057c25bdd1cab375b
clamba15 <cl@customcraftsoftware.com>
2024-06-22 20:40:05 -0700

    pricing

--- packages/mui-components/src/components/Pricing/Pricing.tsx
+++ packages/mui-components/src/components/Pricing/Pricing.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 interface PricingProps { 
   title: string;
   children?: React.ReactNode;
-  pricing: string;
+  pricing: number;
   to: string;
   buttonText: string;
 }
@@ -14,7 +14,11 @@ interface PricingProps {
 const style : SxProps<Theme> = { 
   width: 200,
   height: 200,
-
+  padding: 2,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
 }
 
 const Pricing: React.FC<PricingProps> = ({title = 'title', pricing = 0, to="/", buttonText="Submit", children}) => {
@@ -23,7 +27,7 @@ const Pricing: React.FC<PricingProps> = ({title = 'title', pricing = 0, to="/", 
         <Typography variant='h1'>{title}</Typography>
         <Typography variant='h3'>$ {pricing}</Typography>
         {children}
-        <Button component={Link} to={to}>{buttonText}</Button>
+        <Button>{buttonText}</Button>
     </Paper>
   );
 };
